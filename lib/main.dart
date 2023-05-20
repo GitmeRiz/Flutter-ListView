32b6c79d7ac8cc3e7d95040f8ee962775c305b57
GitmeRiz <gitmeriz@gmail.com>
2023-05-21 06:47:11 +0700

    Update main.dart, make secondPage.dart, make statefulWidget.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/secondPage.dart';
+import 'package:flutter_application_1/statefulWidget.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -14,27 +18,53 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: const Text("RizApp")),
-          floatingActionButton: FloatingActionButton(onPressed: () {}, child: Icon(Icons.no_transfer)),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(labelText: "Username"),
+    return Scaffold(
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            ListTile(
+              title: Text("Home")),
+            ListTile(
+              title: Text("Stateful Widget"),
+              onTap: () {
+                Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const stafulWidget(),
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: ("Password")),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(onPressed: () {}, child: Text("Login"))
-              ],
-            ),
-          )),
-    );
+              );
+              },
+            )
+          ],
+        )),
+        appBar: AppBar(title: const Text("RizApp")),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {}, child: Icon(Icons.no_transfer)),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: "Username"),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: ("Password")),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const MyWidget(
+                          parameter: "Riz Param",
+                        ),
+                      ),
+                    );
+                  },
+                  child: Text("Login"))
+            ],
+          ),
+        ));
   }
 }
